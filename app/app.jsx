02bd6ac017c1d027b65ef6258c12304d19131e7c
julia-jel <44364383+julia-jel@users.noreply.github.com>
2019-07-17 17:07:06 +0300

    Create app.jsx

---
+++ app/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* Import Components */
+import DrumMachineBox from './components/DrumMachineBox';
+
+ReactDOM.render(<DrumMachineBox />, document.getElementById("root"))
