57b1cc09104f7c74e22d23b11951d2ef14d187a6
julia-jel <44364383+julia-jel@users.noreply.github.com>
2019-07-17 17:01:49 +0300

    Create Manual.jsx

---
+++ app/components/Manual.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+//function to toggle an argument:
+
+const toggleInfo = function(s) {
+  return (s === " ") ? "Adjust the tempo, then hold the key on the keyboard." : " "
+}
+
+//button to show the user manual: 
+
+class Manual extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      manual: " "
+    };
+    this.handleInfoClick = this.handleInfoClick.bind(this)
+  };
+  
+  handleInfoClick() {
+    this.setState({
+      manual: toggleInfo(this.state.manual)
+    })
+  }
+  
+  render() {
+    return (
+      <div id="header-section">
+        <h3 id="machine-header">80s Drum Machine</h3>
+        <i className="fas fa-info" onClick={this.handleInfoClick}></i>
+        <div id="manual-info">
+          {this.state.manual}
+        </div>
+      </div>
+    )
+  }
+};
+
+module.exports = Manual;
